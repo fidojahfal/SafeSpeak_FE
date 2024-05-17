f72b13633a6062e2d00d83f5ee05a2434a6fd456
Fido Prayoga <fidojahfalprayoga@gmail.com>
2024-05-17 20:25:35 +0700

    Add register function to api utils

--- src/utils/api.js
+++ src/utils/api.js
@@ -37,6 +37,40 @@ export async function login({ username, password }) {
   return token;
 }
 
+export async function register({
+  username,
+  password,
+  name,
+  nim,
+  email,
+  jurusan,
+  telepon,
+}) {
+  const response = await fetch(`${BASE_URL}/users/register`, {
+    method: 'POST',
+    body: JSON.stringify({
+      username,
+      password,
+      name,
+      nim,
+      email,
+      jurusan,
+      telepon,
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  const responseJson = await response.json();
+
+  if (responseJson.message !== 'Success') {
+    alert(responseJson.message);
+    return { error: true, data: null };
+  }
+
+  return 'Your account successfully registered';
+}
+
 // get user by id
 export async function getUser(id) {
   const response = await fetchWithToken(`${BASE_URL}/users/${id}`);
