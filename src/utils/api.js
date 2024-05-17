acd8da82adc70394a720cd97194858f492684109
Fido Prayoga <fidojahfalprayoga@gmail.com>
2024-05-17 20:14:12 +0700

    Change utils file name to api.js and base url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,8 @@
-const BASE_URL = "http://localhost:3000/api/v1/users";
+const BASE_URL = 'http://localhost:3000/api/v1';
 
 // hardcode get token
 const token =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2NDY0Mjc1M2M5ODcyZGU5MDI4ZDczMCIsImlhdCI6MTcxNTk0ODQwMCwiZXhwIjoxNzE1OTUyMDAwfQ.HGT8moP0Tj7KDUcetzmNgYDGV8XU3-nZLmpIRCmzueE";
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2NDY0Mjc1M2M5ODcyZGU5MDI4ZDczMCIsImlhdCI6MTcxNTk0ODQwMCwiZXhwIjoxNzE1OTUyMDAwfQ.HGT8moP0Tj7KDUcetzmNgYDGV8XU3-nZLmpIRCmzueE';
 
 // fetchWithToken
 async function fetchWithToken(url, options = {}) {
@@ -17,7 +17,7 @@ async function fetchWithToken(url, options = {}) {
 
 // get user by id
 export async function getUser(id) {
-  const response = await fetchWithToken(`${BASE_URL}/${id}`);
+  const response = await fetchWithToken(`${BASE_URL}/users/${id}`);
   const responseJson = await response.json();
 
   if (responseJson.data.user === null) {
