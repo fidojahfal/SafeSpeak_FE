71efb5c89d025f7d8e689a699bf5c5702ac37add
Fido Prayoga <fidojahfalprayoga@gmail.com>
2024-05-19 20:22:13 +0700

    Change get token in fetchwithtoken so now authorization can get token from localstorage

--- src/utils/api.js
+++ src/utils/api.js
@@ -6,7 +6,7 @@ async function fetchWithToken(url, options = {}) {
     ...options,
     headers: {
       ...options.headers,
-      Authorization: `Bearer ${getAccessToken}`,
+      Authorization: `Bearer ${getAccessToken()}`,
     },
   });
 }
