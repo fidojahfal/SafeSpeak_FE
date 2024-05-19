1a5a669f6dfaa3d54bd67bc6936393e0eb94be3d
Fido Prayoga <fidojahfalprayoga@gmail.com>
2024-05-19 19:45:57 +0700

    Add Input component to login

--- src/components/Login Components/LoginInput.jsx
+++ src/components/Login Components/LoginInput.jsx
@@ -1,14 +1,26 @@
 // LoginInput.jsx
-import React from "react";
-import PropTypes from "prop-types";
-import useInput from "../hooks/useInput";
+import React from 'react';
+import PropTypes from 'prop-types';
+import useInput from '../hooks/useInput';
+import Input from '../form/Input';
 
 function LoginInput({ login }) {
-  const [username, onUsernameChange] = useInput("");
-  const [password, onPasswordChange] = useInput("");
+  const [username, onUsernameChange] = useInput('');
+  const [password, onPasswordChange] = useInput('');
 
   return (
     <form className="login-input">
+      <Input
+        labelAndId="username"
+        type="text"
+        val={username}
+        onChangeHandler={onUsernameChange}
+        placeholder="Username"
+      >
+        <p>
+          <b>Username</b>
+        </p>
+      </Input>
       <p>
         <b>Username</b>
       </p>
@@ -29,7 +41,11 @@ function LoginInput({ login }) {
         onChange={onPasswordChange}
         placeholder="Password"
       />
-      <button className="btn" type="button" onClick={() => login({ username, password })}>
+      <button
+        className="btn"
+        type="button"
+        onClick={() => login({ username, password })}
+      >
         Login
       </button>
     </form>
