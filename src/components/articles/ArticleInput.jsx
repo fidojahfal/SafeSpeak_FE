976e2a69810b6bb9b0406fe23261d8968676cb9d
Shabrina Nurmalitasari <shabrinanurmalitasari@gmail.com>
2024-06-02 18:31:43 +0700

    initial cloudinary at article

--- src/components/articles/ArticleInput.jsx
+++ src/components/articles/ArticleInput.jsx
@@ -7,6 +7,32 @@ import { useInput } from "../../hooks/useInput";
 function ArticleInput({ title, content, image, isEdit, submitHandler }) {
   const [titleInput, onTitleInputChange] = useInput(title);
   const [contentInput, onContentInputChange] = useInput(content);
+  const [imageInput, onImageInputChange] = useInput(image);
+
+  const uploadImage = async () => {
+    const formData = new FormData();
+    formData.append("file", imageInput);
+    formData.append("upload_preset", "alxssoyv");
+
+    const response = await fetch(
+      "https://api.cloudinary.com/v1_1/dcgp7jyh2/upload",
+      {
+        method: "POST",
+        body: formData,
+      }
+    );
+    console.log(response);
+  };
+
+  const onSubmitMock = async (event) => {
+    event.preventDefault();
+    await uploadImage();
+    console.log({
+      title: titleInput,
+      content: contentInput,
+      image: "temporary",
+    });
+  };
 
   const onSubmitHandler = (event) => {
     event.preventDefault();
@@ -18,7 +44,7 @@ function ArticleInput({ title, content, image, isEdit, submitHandler }) {
   };
 
   return (
-    <form onSubmit={onSubmitHandler}>
+    <form onSubmit={onSubmitMock}>
       <h5 className="mb-3">{isEdit ? "Ubah Artikel" : "Buat Artikel"}</h5>
       <div className="row gy-3 gx-4">
         <Input
@@ -44,14 +70,15 @@ function ArticleInput({ title, content, image, isEdit, submitHandler }) {
         </Input>
         <div className="col-12">
           <p className="mb-2 form-blue-label-semibold">Lampiran Gambar</p>
-          <Button marginClass="d-flex align-items-center">
+          {/* <Button marginClass="d-flex align-items-center">
             <IconContext.Provider value={{ size: "25px" }}>
               <div>
                 <MdOutlineFileUpload />
               </div>
             </IconContext.Provider>
             <p className="m-0 ms-2">Telusuri File</p>
-          </Button>
+          </Button> */}
+          <input type="file" onChange={onImageInputChange} />
         </div>
         <div className="d-flex justify-content-center">
           <Button type="submit">Buat Laporan</Button>
