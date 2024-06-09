6c094b4f1f3f5a48c59ce940f0e58f70211fa5b8
Shabrina Nurmalitasari <shabrinanurmalitasari@gmail.com>
2024-06-09 16:31:59 +0700

    create a reusable components, currently used in create report & create article page

--- src/pages/UpdateArticlePage.jsx
+++ src/pages/UpdateArticlePage.jsx
@@ -7,6 +7,7 @@ import {
   asyncUpdateArticleDetail,
 } from "../states/articleDetail/action";
 import { useEffect } from "react";
+import GeneralCard from "../components/shared/GeneralCard";
 
 function UpdateArticlePage() {
   const dispatch = useDispatch();
@@ -35,27 +36,13 @@ function UpdateArticlePage() {
   }
 
   return (
-    <section className="bg-blue-100 p-4">
-      <div className="row">
-        <div className="col-lg-auto mx-2 mb-4 mt-1">
-          <Link onClick={() => navigate(-1)}>
-            <img src="/icons/arrow-left-circle-fill.svg" alt="arrow-left" />
-          </Link>
-        </div>
-        <div className="col-lg-11">
-          <Alert />
-          <div className="col-lg card p-3">
-            <div className="card-body">
-              <ArticleInput
-                {...articleDetail}
-                isEdit={true}
-                submitHandler={onUpdateArticleHandler}
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
+    <GeneralCard>
+      <ArticleInput
+        {...articleDetail}
+        isEdit={true}
+        submitHandler={onUpdateArticleHandler}
+      />
+    </GeneralCard>
   );
 }
 
