99eaac97907a6b2a30ecb74edd5854c1fdef837f
Shabrina Nurmalitasari <shabrinanurmalitasari@gmail.com>
2024-06-11 17:55:40 +0700

    navigate jelajahi artikel button

--- src/components/home/ArticleSection.jsx
+++ src/components/home/ArticleSection.jsx
@@ -1,13 +1,13 @@
-import Button from '../form/Button';
-import ArticleMiniCard from './ArticleMiniCard';
+import Button from "../form/Button";
+import ArticleMiniCard from "./ArticleMiniCard";
 
-function ArticleSection({ role, articles, onDetail }) {
+function ArticleSection({ role, articles, onDetail, navigate }) {
   return (
     <section className="mb-5">
-      <div className="row" style={{ '--bs-gutter-x': '0' }}>
+      <div className="row" style={{ "--bs-gutter-x": "0" }}>
         <div className="col-lg-6">
           <img
-            src={role ? '/img/Beranda-Dosen-2.png' : '/img/Beranda-Siswa-2.png'}
+            src={role ? "/img/Beranda-Dosen-2.png" : "/img/Beranda-Siswa-2.png"}
             alt="Illustration of a girl looking at laptop"
             className="img-fluid home-article-image"
           />
@@ -15,23 +15,28 @@ function ArticleSection({ role, articles, onDetail }) {
         <div className="col-lg-6 p-5">
           <h1 className="text-primary fw-bold text-lg-end text-center">
             {role
-              ? 'Edukasi siswa mengenai kekerasan seksual, bullying, dan prosedur penanganannya.'
-              : 'Dapatkan informasi seputar kekerasan seksual, bullying, dan proses penanganannya disini.'}
+              ? "Edukasi siswa mengenai kekerasan seksual, bullying, dan prosedur penanganannya."
+              : "Dapatkan informasi seputar kekerasan seksual, bullying, dan proses penanganannya disini."}
           </h1>
           <p className="fs-5 text-lg-end text-center mt-3 ">
             {role
-              ? 'Kontribusi dalam membuat artikel sebagai informasi edukasi untuk siswa.'
-              : 'Jelajahi artikel seputar edukasi, proses penanganan, dan berita terbaru.'}
+              ? "Kontribusi dalam membuat artikel sebagai informasi edukasi untuk siswa."
+              : "Jelajahi artikel seputar edukasi, proses penanganan, dan berita terbaru."}
           </p>
           <div className="d-flex justify-content-lg-end justify-content-center">
-            <Button marginClass="fw-semibold">Jelajahi Artikel</Button>
+            <Button
+              marginClass="fw-semibold"
+              onClickHandler={() => navigate("/articles")}
+            >
+              Jelajahi Artikel
+            </Button>
           </div>
         </div>
       </div>
       <p className="text-secondary text-center">Beberapa artikel terkini</p>
       <div
         className="d-flex justify-content-center align-items-center flex-sm-column flex-md-row flex-wrap"
-        style={{ gap: '3rem' }}
+        style={{ gap: "3rem" }}
       >
         {articles.length !== 0 ? (
           articles
