4537219f133fe4ef5e766826042968ed8f8eeb59
Giovillando <121701082+Giovillando@users.noreply.github.com>
2024-06-15 18:26:49 +0700

    melakukan refactor pada login dan register

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import RegisterInput from "../components/register/RegisterInput";
 import { useDispatch } from "react-redux";
 import { asyncRegisterUser } from "../states/user/action.js";
-import { Link } from "react-router-dom";
+import GeneralRegister from "../components/shared/GeneralRegister.jsx";
 
 function RegisterPage() {
   const navigate = useNavigate();
@@ -33,43 +33,9 @@ function RegisterPage() {
   };
 
   return (
-    <div className="container-fluid min-vh-100 d-flex flex-column">
-      <div className="row flex-grow-1 align-items-center">
-        <div className="col-lg-6 d-flex flex-column justify-content-center p-5 position-relative mb-5">
-          <div className="d-flex align-items-center ps-2 position-absolute top-0 start-0 mt-3 ms-3 mb-5">
-            <img
-              src="/logo/SafeSpeak-Logo.svg"
-              alt="SafeSpeak Logo"
-              className="me-2"
-              style={{ width: "35px" }}
-            />
-            <p className="fs-5 fw-bold text-primary m-0">SafeSpeak</p>
-          </div>
-          <div className="py-5">
-            <h4 className="text-center text-primary fw-bold">Sign Up</h4>
-            <p className="text-center text-primary">
-              Silahkan daftar untuk bisa mengakses fitur Lapor
-            </p>
-            <RegisterInput register={onRegister} />
-            <div className="d-flex align-items-center mt-4 justify-content-center">
-              <p className="m-0 text-secondary">Sudah memiliki akun?</p>
-              <Link to="/login" className="ms-1 text-secondary fw-bold">
-                Login
-              </Link>
-            </div>
-          </div>
-        </div>
-        <div className="col-lg-6 d-none d-lg-flex justify-content-center align-items-center bg-light-custom position-fixed top-0 end-0 min-vh-100 gx-0">
-          <div className="register-image container w-125 h-125 d-flex justify-content-center align-items-center">
-            <img
-              src="/img/SignIn.png"
-              className="custom-img-size"
-              alt="Register"
-            />
-          </div>
-        </div>
-      </div>
-    </div>
+    <GeneralRegister>
+      <RegisterInput register={onRegister} />
+    </GeneralRegister>
   );
 }
 
